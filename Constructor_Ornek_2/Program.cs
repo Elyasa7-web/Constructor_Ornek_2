29748d21c919b3d35252c6ab7361866c9aab963c
fb_20 <fb_20@ELYASA>
2024-11-24 14:03:36 +0300

    Add project files.

---
+++ Constructor_Ornek_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Ornek_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kimlik kml = new Kimlik();
+            kml.AD = "Elyasa";
+            kml.SOYAD = "Kapucu";
+            kml.MEMLEKET = "Ordu";
+            kml.YAŞ = 22;
+            kml.CINSIYET = 'E';
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.MEMLEKET);
+            Console.WriteLine(kml.YAŞ);
+            Console.WriteLine(kml.CINSIYET);
+            Console.Read();
+        }
+    }
+}
